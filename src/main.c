e94d30888b296868e8758026e69a9227a3fc0cbf
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-09-26 19:16:08 -0400

    Setting up main to accept 1 command line argument - file path for the program to load

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+  if (argc == 1) {
+    // TODO: implement REPL
+    printf("Usage: ./carbon <filename>");
+  }
+  else if (argc == 2) {
+
+    // set single locale for all purposes
+    setlocale(LC_ALL, "");
+
+    // TODO: Allow filenames with unicode characters
+    // e.g., filename with multi-byte chinese characters
+    const char * filename = argv[1];
+
+
+
+  }
+
   return 0;
 }
