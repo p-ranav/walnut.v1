fd81f2a97ef60883760f712110a88c3050525eea
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-09-26 20:13:27 -0400

    Passing pointer to buffer to correctly set value

--- src/utils/file.c
+++ src/utils/file.c
@@ -1,18 +1,18 @@
-#include <file_utils.h>
+#include <file.h>
 
-long read_file(const char *filename, char *buffer)
+long read_file(const char *file_path, char **buffer)
 {
   FILE *file;
-  long file_length;
+  long file_size;
 
-  file = fopen(filename, "rb"); // Open the file in binary mode
+  file = fopen(file_path, "rb"); // Open the file in binary mode
   fseek(file, 0, SEEK_END);     // Jump to the end of the file
-  file_length = ftell(file);    // Get the current byte offset in the file
+  file_size = ftell(file);    // Get the current byte offset in the file
   rewind(file);                 // Jump back to the beginning of the file
 
-  buffer = (char *)malloc((file_length + 1) * sizeof(char)); // Enough memory for file + \0
-  fread(buffer, file_length, 1, file);                       // Read in the entire file
+  *buffer = (char *)malloc((file_size + 1) * sizeof(char)); // Enough memory for file + \0
+  fread(*buffer, file_size, 1, file);                       // Read in the entire file
   fclose(file);                                              // Close the file
 
-  return file_length;
+  return file_size;
 }
--- src/utils/file.h
+++ src/utils/file.h
@@ -6,6 +6,6 @@
 // opens the file pointed to by filename
 // reads the entire file into buffer
 // returns file size
-long read_file(const char *filename, char *buffer);
+long read_file(const char *filename, char **buffer);
 
 #endif
