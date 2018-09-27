bd93cd0e41bb46a579b6dc3605f64a9f027cc89b
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-09-26 20:13:07 -0400

    Invoking lexer::tokenize in main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#include <file.h>
+#include <macros.h>
+#include <lexer.h>
+
 #include <stdio.h> 
 #include <string.h>
 #include <stdlib.h>
@@ -7,7 +11,7 @@ int main(int argc, char *argv[]) {
 
   if (argc == 1) {
     // TODO: implement REPL
-    printf("Usage: ./carbon <filename>");
+    trace("Usage: ./carbon <filename>");
   }
   else if (argc == 2) {
 
@@ -16,9 +20,15 @@ int main(int argc, char *argv[]) {
 
     // TODO: Allow filenames with unicode characters
     // e.g., filename with multi-byte chinese characters
-    const char * filename = argv[1];
+    const char * file_path = argv[1];
 
+    // Read from file and retrieve buffer of characters
+    char * buffer = NULL;
+    long file_size = read_file(file_path, &buffer);
+    trace("%s (%ld bytes)\n", file_path, file_size);
 
+    // Tokenize the buffer of characters
+    tokenize(file_size, buffer);
 
   }
 
