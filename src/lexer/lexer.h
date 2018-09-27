2370eb3d8a498c303167cc0ac8c2a9ad3cb8eeab
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-09-26 20:14:17 -0400

    Lexer tokenize takes a file_size (bytes) and a buffer of bytes. That's all I need.

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,6 +1,20 @@
 #ifndef LEXER_H
 #define LEXER_H
+#include <token.h>
+#include <utf8.h>
+#include <list.h>
+#include <macros.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// The lexer needs to do the following:
+// (1) Take the sequence of bytes in the input buffer
+// (2) correctly parsing unicode characters (UTF-8 encoding)
+// (3) tokenize the stream of characters (build a list of tokens)
+// (4) return the list of tokens
+// Tokens can be of different categories (see token.h)
+list_t * tokenize(long buffer_size, char * buffer);
 
 #endif
