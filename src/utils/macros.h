fcbe8f1de0a93899ea63ad5d913a8aeb1ef04121
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-09-29 09:23:59 -0400

    Removed trace and error macros. Added deallocate macro

--- src/utils/macros.h
+++ src/utils/macros.h
@@ -1,12 +1,12 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define trace printf
-#define error printf
-
 #define deallocate(pointer) \
   if(pointer)\
     free(pointer);\
   pointer = NULL;
+
+/* Lexer macros */
+
 
 #endif
