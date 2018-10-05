a3a81b853e097384acccb2c879827f645adfb3f3
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-05 19:29:40 -0400

    Added comments to float and double ast node structs

--- src/parser/float_node.h
+++ src/parser/float_node.h
@@ -4,16 +4,26 @@
 #include <node.h>
 #include <macros.h>
 
+/* float_node is an AST node type to represent
+   a single-precision floating point number. So,
+   the value is a float type.
+*/
 typedef struct
 {
   enum node_type_t type; /* type = FLOAT */
   float value;           /* value = float32 value */
 } float_node;
 
+/* Construct a float_node and set its type to FLOAT */
 float_node * float_construct(float value);
 
+/* Overriding method that returns the FLOAT node_type value */
 enum node_type_t float_type(float_node * object);
+
+/* Overriding method that prints the value of the float with printf */
 void float_print(float_node * object);
+
+/* Free float_node pointer - simple since it has no contained pointers */
 void float_destruct(float_node * object);
 
 #endif
