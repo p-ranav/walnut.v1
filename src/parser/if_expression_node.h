34174701a71a342a8bc6d347cefedac587271b0b
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-05 20:56:20 -0400

    Comments for if expression node type

--- src/parser/if_expression_node.h
+++ src/parser/if_expression_node.h
@@ -5,18 +5,38 @@
 #include <macros.h>
 #include <block_node.h>
 
+/* if_expression_node is an AST node that represents an if-else
+   block. There is no else-if support as of right now. So, this
+   AST node represents code that looks like this:
+
+   [...]
+   if (a % 2 == 0) { return true; } else { return false; }
+   [...]
+
+   The if expression starts with the keyword 'if', followed
+   by a condition expression. After the condition, we have the
+   consequence block of code and optionally an alternative block
+   of code that is executed when the condition is false.
+
+*/
 typedef struct
 {
-  enum node_type_t type;    /* type = IF_EXPRESSION */
-  node * condition;         /* condition expression */
-  block_node * consequence;
-  block_node * alternative;
+  enum node_type_t type;       /* type = IF_EXPRESSION */
+  node * condition;            /* condition expression */
+  block_node * consequence;    /* block of code following the if expression */
+  block_node * alternative;    /* block of code following the else expression. Could be NULL */
 } if_expression_node;
 
+/* Construct an if expression - initialize condition, consequence and alternative nodes */
 if_expression_node * if_expression_construct();
 
+/* return IF_EXPRESSION node type */
 enum node_type_t if_expression_type(if_expression_node * object);
+
+/* print an if expression, e.g., if (...) { ... } else { ... } */
 void if_expression_print(if_expression_node * object);
+
+/* free an if expression node - clean up block nodes and condition expression */
 void if_expression_destruct(if_expression_node * object);
 
 #endif
