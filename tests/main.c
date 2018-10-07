eddbb0fc3ae4551cf2dc629367df59bb925b17d3
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-07 10:09:20 -0400

    Added lexer test for boolean

--- tests/main.c
+++ tests/main.c
@@ -1,7 +1,8 @@
-#include <lexer_token_boolean.h>
+#include <test_lexer_token_boolean.h>
 
 TEST_LIST
 {
   TEST_RUN(lexer_token_boolean_true);
+  TEST_RUN(lexer_token_boolean_false);
   GENERATE_TEST_REPORT();
 }
