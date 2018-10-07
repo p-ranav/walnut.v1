db7cb35fc7c56d72f39db98c761e25116d044861
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-07 10:56:15 -0400

    Added basic check for empty input

---
+++ tests/test_lexer_token_eof.h
@@ -0,0 +1,52 @@
+#ifndef TEST_LEXER_TOKEN_EOF
+#define TEST_LEXER_TOKEN_EOF
+
+#include <lexer.h>
+#include <macros.h>
+#include <list.h>
+
+TEST_CASE(lexer_token_eof)
+{
+  /* Declarations */
+  const char *file_path;
+  list_t *tokens;
+  char *buffer;
+  size_t file_size;
+  size_t i;
+  token expected_tokens[1] = { TOKEN_END_OF_FILE };
+  const char * test_label;
+
+  /* Initialization */
+  file_path = __FILE__;
+  buffer = "";
+  file_size = strlen(buffer);
+  i = 0;
+
+  /* Log test name */
+  DECLARE_TEST("Lexer recognizes the input \"\" as {TOKEN_END_OF_FILE}");
+
+  /* Tokenize the buffer of characters */
+  tokens = lexer_tokenize(file_path, file_size, buffer);
+
+  /* post-processing step in lexical analysis */
+  lexer_post_process(tokens);
+
+  while (i < arrlen(expected_tokens))
+  {
+    /* get pointer to token and print token type and value */
+    struct token_t * token = ((struct token_t *)(list_at(tokens, i))->val);
+
+    /* check result list of lexer tokens */
+    TEST_CHECK(expected_tokens[i] == token->type);
+
+    /* if we're here, all good so far */
+    i = i + 1;
+  }
+
+  /* cleanup after test */
+  lexer_destroy(tokens);
+
+  RETURN_TEST_SUCCESS();
+}
+
+#endif
