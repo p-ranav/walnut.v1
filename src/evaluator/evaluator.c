077b98a5737aab7c01adbf29707cf550e9951768
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-13 11:34:37 -0400

    Added eval function for boolean

--- src/evaluator/evaluator.c
+++ src/evaluator/evaluator.c
@@ -2,9 +2,11 @@
 
 /* Parser headers */
 #include <integer_node.h>
+#include <bool_node.h>
 
 /* Object model headers */
 #include <integer_object.h>
+#include <bool_object.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +18,9 @@ object * eval(node * ast_node)
   {
   case INTEGER:
     return eval_integer(ast_node);
+    break;
+  case BOOLEAN:
+    return eval_boolean(ast_node);
     break;
   default:
     return NULL;
@@ -36,4 +41,19 @@ object * eval_integer(node * ast_node)
   INTEGER_AS_OBJECT->inspect = (const char *(*)(void *))integer_object_inspect;
   INTEGER_AS_OBJECT->destruct = (void(*)(void *))integer_object_destruct;
   return object_construct(obj, INTEGER_AS_OBJECT);
+}
+
+object * eval_boolean(node * ast_node)
+{
+  /* declarations */
+  boolean_object * obj;
+  object_interface * BOOLEAN_AS_OBJECT;
+
+  obj = boolean_object_construct(((integer_node *)ast_node->instance)->value);
+
+  BOOLEAN_AS_OBJECT = allocate(object_interface, 1);
+  BOOLEAN_AS_OBJECT->type = (enum object_type_t(*)(void *))boolean_object_type;
+  BOOLEAN_AS_OBJECT->inspect = (const char *(*)(void *))boolean_object_inspect;
+  BOOLEAN_AS_OBJECT->destruct = (void(*)(void *))boolean_object_destruct;
+  return object_construct(obj, BOOLEAN_AS_OBJECT);
 }
--- src/evaluator/evaluator.h
+++ src/evaluator/evaluator.h
@@ -7,5 +7,6 @@ object * eval(node * ast_node);
 
 /* eval functions for ast node types */
 object * eval_integer(node * ast_node);
+object * eval_boolean(node * ast_node);
 
 #endif
