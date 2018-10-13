7ce8a32bb6eaf2ae66793dba23b92fe3b2488c30
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-13 09:57:41 -0400

    Added object_type_t enum similar to lexer token types. Easy to add string version of object type this way

--- src/object/object_type.h
+++ src/object/object_type.h
@@ -1,6 +1,17 @@
 #ifndef OBJECT_TYPE_H
 #define OBJECT_TYPE_H
 
+/* List of all object types in the interpreter object model */
+#define OBJECT_LIST \
+  OBJECT(INTEGER, "integer") \
 
+/* Enum of tokens */
+enum object_type_t
+{
+#define OBJECT(label, string) OBJECT_##label,
+  OBJECT_LIST
+#undef OBJECT
+  OBJECT_INVALID
+};
 
 #endif
