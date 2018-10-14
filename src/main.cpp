8adfd7d387e17166a83c36ff160cacd913de6f9a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-14 12:36:49 -0400

    Added main source file to invoke lexer

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <lexer.hpp>
+
+#include <iostream>
+#include <string>
+#include <clocale>
+
+int main(int argc, char *argv[])
+{
+
+  if (argc == 1)
+  {
+    /* report usage */
+    std::cerr << "Usage: ./ulang <filename>" << std::endl;
+    return 0;
+  }
+  else if (argc == 2)
+  {
+
+    /* set single locale for all purposes */
+    setlocale(LC_ALL, "");
+
+    /* TODO: Allow filenames with unicode characters */
+    /* e.g., filename with multi-byte chinese characters */
+    std::string file_path = argv[1];
+
+    /* Tokenize the buffer of characters */
+    lexer::tokenize(file_path);
+
+  }
+
+  return 0;
+}
