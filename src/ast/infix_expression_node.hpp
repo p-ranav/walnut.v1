6ee7b8d4c15043527cd226a9bc34b6500e1c7792
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-14 23:23:48 -0400

    Infix expression AST node type

---
+++ src/ast/infix_expression_node.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <node.hpp>
+#include <token.hpp>
+
+struct AstInfixExpressionNode : AstNode
+{
+  AstNodePtr left;
+  TokenType infix_operator;
+  AstNodePtr right;
+
+  explicit AstInfixExpressionNode(TokenType infix_operator) : AstNode(INFIX_EXPRESSION),
+    left(nullptr),
+    infix_operator(infix_operator),
+    right(nullptr) {}
+};
+
+typedef std::shared_ptr<AstInfixExpressionNode> AstInfixExpressionNodePtr;
