3d78cf5e5ba251f1fea20a06744252f7269a2765
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-16 22:13:09 -0400

    Implemented built-in function for array length

--- src/evaluator/builtin_functions.cpp
+++ src/evaluator/builtin_functions.cpp
@@ -9,6 +9,11 @@ ObjectPtr Evaluator::len(std::vector<ObjectPtr> arguments)
       StringObjectPtr string_object = std::dynamic_pointer_cast<StringObject>(arguments[0]);
       return std::make_shared<IntegerObject>(static_cast<int>(string_object->value.length()));
     }
+    else if (arguments[0]->type == ObjectType::ARRAY)
+    {
+      ArrayObjectPtr array_object = std::dynamic_pointer_cast<ArrayObject>(arguments[0]);
+      return std::make_shared<IntegerObject>(static_cast<int>(array_object->elements.size()));
+    }
   }
   return std::make_shared<NullObject>();
 }
