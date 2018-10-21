5e5dfe915070507745c7babe3fe1f12e42884cd7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-20 20:49:49 -0400

    Merged with old history and cleaned up directories

--- src/object/object_type.c
+++
@@ -1,9 +0,0 @@
-#include <object_type.h>
-
-/* List of object type strings - Used for debug printing */
-const char *const object_strings[] = {
-#define OBJECT(label, string) string,
-    OBJECT_LIST
-#undef OBJECT
-    "invalid" 
-};
