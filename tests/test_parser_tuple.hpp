61f73894976e92ec9b0536abc888688335b3f3eb
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-10-25 18:27:04 -0400

    Parser unit test for tuple of strings

--- tests/test_parser_tuple.hpp
+++ tests/test_parser_tuple.hpp
@@ -68,3 +68,25 @@ TEST_CASE("The parser can parse a tuple of doubles '(1.0, 2.5, 3.9)'", "[lexer]"
   REQUIRE(tuple->elements[1]->ToString() == "2.50");
   REQUIRE(tuple->elements[2]->ToString() == "3.90");
 }
+
+TEST_CASE("The parser can parse a tuple of strings '(\"x\", \"y\", \"z\")'", "[lexer]")
+{
+  setlocale(LC_ALL, "");
+  EnvironmentPtr environment = std::make_shared<Environment>();
+  String filename = "";
+  String buffer = "(\"x\", \"y\", \"z\")";
+  Lexer lexer(filename, buffer);
+  lexer.Tokenize();
+  Parser parser(lexer.tokens);
+  parser.ParseProgram();
+  REQUIRE(parser.statements.size() == 1);
+  REQUIRE(parser.statements[0]->type == Node::Type::TUPLE);
+  REQUIRE(parser.statements[0]->ToString() == "(\"x\", \"y\", \"z\")");
+  TupleNodePtr tuple = std::dynamic_pointer_cast<TupleNode>(parser.statements[0]);
+  REQUIRE(tuple->elements.size() == 3);
+  for (auto& element : tuple->elements)
+    REQUIRE(element->type == Node::Type::STRING_LITERAL);
+  REQUIRE(tuple->elements[0]->ToString() == "\"x\"");
+  REQUIRE(tuple->elements[1]->ToString() == "\"y\"");
+  REQUIRE(tuple->elements[2]->ToString() == "\"z\"");
+}
