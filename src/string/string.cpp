63b4a6bb8c8edc361cc546583f5b710eaab5e797
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-11-10 19:28:50 -0500

    Optimized string library following cppcheck results

--- src/string/string.cpp
+++ src/string/string.cpp
@@ -15,7 +15,7 @@ size_t strlcpy(char *dst, const char *src, size_t destination_size)
   return length;
 }
 
-std::string format(const std::string format_string, ...)
+String format(StringConst format_string, ...)
 {
   int n = ((int)format_string.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
   std::unique_ptr<char[]> formatted;
@@ -32,13 +32,13 @@ std::string format(const std::string format_string, ...)
     else
       break;
   }
-  return std::string(formatted.get());
+  return String(formatted.get());
 }
 
 // Python-style String slicing
-std::string slice(std::string input_string, int start_index, int end_index, int step)
+String slice(StringConstRef input_string, int start_index, int end_index, int step)
 {
-  std::string result;
+  String result;
   if (end_index == INT_MAX)
   { // if end index is default, i.e., till end of string
     if (start_index < 0)
@@ -57,7 +57,7 @@ std::string slice(std::string input_string, int start_index, int end_index, int 
     }
   }
   else
-  { // end_index is not std::string::npos
+  { // end_index is not String::npos
     if (end_index < 0)
     { // if end_index is negative, start from start_index and go till (end_of_string + end_index)
       for (int i = start_index; i < static_cast<int>(input_string.size()) + end_index; i = i + step)
@@ -79,27 +79,28 @@ std::string slice(std::string input_string, int start_index, int end_index, int 
 // split string based on a delimiter string
 // supports multi-character delimiter
 // returns a vector of substrings after split
-std::vector<std::string> split(std::string input_string, std::string delimiter, std::vector<std::string> *result)
+std::vector<String> split(StringConstRef input_string, StringConstRef delimiter, std::vector<String> *result)
 {
-  size_t delimiter_position = input_string.find(delimiter); // check if delimiter is in input string
-  if (delimiter_position != std::string::npos)
+  String current_string(input_string);
+  size_t delimiter_position = current_string.find(delimiter); // check if delimiter is in input string
+  if (delimiter_position != String::npos)
   { // if delimiter position is not end_of_string
     size_t counter_position = 0;
-    std::string split_string = input_string.substr(counter_position, delimiter_position);
+    String split_string = current_string.substr(counter_position, delimiter_position);
     delimiter_position += delimiter.size();
-    std::string split_remaining = input_string.erase(counter_position, delimiter_position);
+    String split_remaining = current_string.erase(counter_position, delimiter_position);
     result->push_back(split_string);
-    std::vector<std::string> result_remaining = split(split_remaining, delimiter, result);
+    split(split_remaining, delimiter, result);
   }
   else
   { // delimiter not in input string. Just add entire input string to result vector
-    result->push_back(input_string);
+    result->push_back(current_string);
   }
   return *result;
 }
 
 // Check if string startswith some character
-bool starts_with(std::string input_string, char c, bool ignore_case)
+bool starts_with(StringConstRef input_string, char c, bool ignore_case)
 {
   bool result = false;
   if (input_string.size() > 0)
@@ -117,12 +118,12 @@ bool starts_with(std::string input_string, char c, bool ignore_case)
 }
 
 // Check if string startswith some substring
-bool starts_with(std::string input_string, std::string starter, bool ignore_case)
+bool starts_with(StringConstRef input_string, StringConstRef starter, bool ignore_case)
 {
   bool result = false;
   if (input_string.size() > 0 && input_string.size() > starter.size())
   {
-    std::string input_copy = input_string;
+    String input_copy = input_string;
     for (size_t i = 0; i < starter.size(); i++)
     {
       char input_character = input_string[i];
@@ -148,7 +149,7 @@ bool starts_with(std::string input_string, std::string starter, bool ignore_case
 }
 
 // Check if string endswith character
-bool ends_with(std::string input_string, char c, bool ignore_case)
+bool ends_with(StringConstRef input_string, char c, bool ignore_case)
 {
   bool result = false;
   if (input_string.size() > 0)
@@ -166,7 +167,7 @@ bool ends_with(std::string input_string, char c, bool ignore_case)
 }
 
 // String contains
-bool contains(std::string input, std::string search_string, bool ignore_case)
+bool contains(String input, String search_string, bool ignore_case)
 {
   bool result = false;
   if (ignore_case)
@@ -174,7 +175,7 @@ bool contains(std::string input, std::string search_string, bool ignore_case)
     std::transform(input.begin(), input.end(), input.begin(), ::toupper);
     std::transform(search_string.begin(), search_string.end(), search_string.begin(), ::toupper);
   }
-  if (input.find(search_string) != std::string::npos)
+  if (input.find(search_string) != String::npos)
   { // search using find algorithm
     result = true;
   }
@@ -182,7 +183,7 @@ bool contains(std::string input, std::string search_string, bool ignore_case)
 }
 
 // Count number of times some search_string appears in input_string
-size_t count(std::string input_string, std::string search_string, bool ignore_case)
+size_t count(String input_string, String search_string, bool ignore_case)
 {
   size_t result = 0;
   if (contains(input_string, search_string, ignore_case))
@@ -199,9 +200,9 @@ size_t count(std::string input_string, std::string search_string, bool ignore_ca
 }
 
 // repeat input string for count number of times, optionally with a connector string
-std::string repeat(std::string input_string, int count_value, std::string connector)
+String repeat(StringConstRef input_string, int count_value, StringConstRef connector)
 {
-  std::string result = "";
+  String result = "";
   while (count_value > 0)
   {
     result += input_string + connector;
@@ -211,18 +212,18 @@ std::string repeat(std::string input_string, int count_value, std::string connec
 }
 
 // returns copy of input string that is all upper case
-std::string upper(std::string input_string)
+String upper(StringConstRef input_string)
 {
-  std::string result;
+  String result;
   result.resize(input_string.size());
   std::transform(input_string.begin(), input_string.end(), result.begin(), ::toupper);
   return result;
 }
 
 // returns copy of input string that is all lower case
-std::string lower(std::string input_string)
+String lower(StringConstRef input_string)
 {
-  std::string result;
+  String result;
   result.resize(input_string.size());
   std::transform(input_string.begin(), input_string.end(), result.begin(), ::tolower);
   return result;
@@ -230,10 +231,10 @@ std::string lower(std::string input_string)
 
 // find and replace substring in input string, optionally for a limited number of times
 // by default, every occurrence of find_string is replaced by replace_string
-std::string replace(std::string input_string, std::string find_string,
-                    std::string replace_string, int replace_count)
+String replace(StringConstRef input_string, StringConstRef find_string,
+               StringConstRef replace_string, int replace_count)
 {
-  std::string result = "";
+  String result = "";
   auto split_vector = split(input_string, find_string);
   auto split_size = split_vector.size();
   auto max_replace_count = split_size - 1;
@@ -275,9 +276,9 @@ std::string replace(std::string input_string, std::string find_string,
 }
 
 // join a vector of strings into a single string
-std::string join(std::vector<std::string> input, std::string connector)
+String join(const std::vector<String>& input, StringConstRef connector)
 {
-  std::string result;
+  String result;
   size_t max_connector_count = input.size() - 1;
   for (auto &s : input)
   {
@@ -291,19 +292,19 @@ std::string join(std::vector<std::string> input, std::string connector)
 }
 
 // bulk find and replace sub-string in input string using translation table
-std::string translate(std::string input_string,
-                      std::map<std::string, std::string> translation_table)
+String translate(StringConstRef input_string,
+                      const std::map<String, String>& translation_table)
 {
-  std::string result = input_string;
+  String result = input_string;
   for (auto &pair : translation_table)
     result = replace(result, pair.first, pair.second);
   return result;
 }
 
 // trim white spaces from the left end of an input string
-std::string ltrim(std::string input_string)
+String ltrim(StringConstRef input_string)
 {
-  std::string result = input_string;
+  String result = input_string;
   result.erase(result.begin(), std::find_if(result.begin(), result.end(), [](int ch) {
                  return !std::isspace(ch);
                }));
@@ -311,9 +312,9 @@ std::string ltrim(std::string input_string)
 }
 
 // trim white spaces from right end of an input string
-std::string rtrim(std::string input_string)
+String rtrim(StringConstRef input_string)
 {
-  std::string result = input_string;
+  String result = input_string;
   result.erase(std::find_if(result.rbegin(), result.rend(), [](int ch) {
                  return !std::isspace(ch);
                })
@@ -323,47 +324,48 @@ std::string rtrim(std::string input_string)
 }
 
 // trim white spaces from either end of an input string
-std::string trim(std::string input_string)
+String trim(StringConstRef input_string)
 {
   return ltrim(rtrim(input_string));
 }
 
 // Returns true if the two input strings are equal
-bool equal(std::string lhs, std::string rhs)
+bool equal(StringConstRef lhs, StringConstRef rhs)
 {
   return (lhs.compare(rhs) == 0);
 }
 
 // generic find function
 // if sub-string is found, returns the index
-// if sub-string is not found, returns std::string::npos
-size_t find(std::string input_string, std::string search_string)
+// if sub-string is not found, returns String::npos
+size_t find(StringConstRef input_string, StringConstRef search_string)
 {
   return input_string.find(search_string);
 }
 
 // finds first occurrence of sub-string in input string
-size_t find_first(std::string input_string, std::string search_string)
+size_t find_first(StringConstRef input_string, StringConstRef search_string)
 {
   return input_string.find_first_of(search_string);
 }
 
 // finds last occurrence of sub-string in input string
-size_t find_last(std::string input_string, std::string search_string)
+size_t find_last(StringConstRef input_string, StringConstRef search_string)
 {
   return input_string.find_last_of(search_string);
 }
 
 // performs std regex search and returns a vector of matched results
-std::vector<std::string> find_regex(std::string input_string, std::string regex_string)
+std::vector<String> find_regex(StringConstRef input_string, StringConstRef regex_string)
 {
-  std::vector<std::string> results;
+  String current_string(input_string);
+  std::vector<String> results;
   std::smatch match_result;
-  while (std::regex_search(input_string, match_result, std::regex(regex_string)))
+  while (std::regex_search(current_string, match_result, std::regex(regex_string)))
   {
     for (auto &m : match_result)
       results.push_back(m);
-    input_string = match_result.suffix();
+    current_string = match_result.suffix();
   }
   return results;
 }
