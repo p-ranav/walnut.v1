e9020de862e47c50f8cc6bb5b4f51896c38c17b0
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2018-11-15 18:45:35 -0500

    iterable.extend support for sets and tuples

--- src/evaluator/builtin_extend.cpp
+++ src/evaluator/builtin_extend.cpp
@@ -15,6 +15,23 @@ ObjectPtr Evaluator::BuiltinExtend(std::vector<ObjectPtr> arguments)
         first->elements.push_back(element);
       return first;
     }
+    else if (arguments[0]->type == ObjectType::SET && arguments[1]->type == ObjectType::SET)
+    {
+      SetObjectPtr first = std::dynamic_pointer_cast<SetObject>(arguments[0]);
+      SetObjectPtr second = std::dynamic_pointer_cast<SetObject>(arguments[1]);
+      for (auto &element : second->elements)
+        first->elements.push_back(element);
+      return first;
+    }
+    else if (arguments[0]->type == ObjectType::TUPLE && arguments[1]->type == ObjectType::TUPLE)
+    {
+      TupleObjectPtr first = std::dynamic_pointer_cast<TupleObject>(arguments[0]);
+      TupleObjectPtr second = std::dynamic_pointer_cast<TupleObject>(arguments[1]);
+      for (auto &element : second->elements)
+        first->elements.push_back(element);
+      return first;
+    }
+    return arguments[0];
   }
   return std::make_shared<NullObject>();
 }
